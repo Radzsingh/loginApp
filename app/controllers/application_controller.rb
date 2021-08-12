d7e1610d16eb2fe3a8793b44bbef2f5c90c76f62
Rajinder Singh <rajinder@popup.page>
2021-08-12 13:10:38 +0100

    Minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def require_user_logged_in!
-    # allows only logged in user
-    redirect_to sign_in_path, alert: 'You must be signed in' if current_user.nil?
-  end
-
   def account_locked?
     user = User.find_by(user_name: params[:user_name])
     user && user.login_attempts > 2 ? true : false
